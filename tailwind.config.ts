3b5fba62a081f053e3f31652f5517a4050afc7df
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2023-08-01 16:32:22 +0300

    Add style for index pages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,17 @@
 import { type Config } from "tailwindcss";
+import  defaultTheme from "tailwindcss/defaultTheme"
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'main': '#474747',
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
